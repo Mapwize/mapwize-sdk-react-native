0b381fe7377e6be39800d9f6b3a75848177c3842
Lakhdar Meftah <lakhdar@mapwize.io>
2020-09-25 10:44:22 +0200

    ci: fix user.name and user.email

--- fixNpmVersion.sh
+++ fixNpmVersion.sh
@@ -6,6 +6,6 @@ git config user.email "$(git log -1 --pretty=format:'%ae')"
 git commit --no-verify -am "$gitMessage" || exit 0
 git tag -d $TRAVIS_TAG
 git tag $TRAVIS_TAG
-git remote add github https://${GITHUB_TOKEN}@github.com/m3ftah/mapwize-sdk-react-native.git > /dev/null 2>&1
+git remote add github https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 git push github :$TRAVIS_TAG
 git push github $TRAVIS_TAG
